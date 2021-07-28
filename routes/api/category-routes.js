096db2377be974d9bbcba72c497930be7507c28c
jacob sierra <jacobisaac22@yahoo.com>
2021-07-28 13:27:17 -0500

    updated category routes.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,17 +4,43 @@ const { Category, Product } = require('../../models');
 
 // categories endpoint 
 // finds all categories
-router.get('/', (req, res) => {
-
+router.get('/', async (req, res) => {
+   try {
+       const allCategories = await Category.findAll({
+           include: [{ model: Product}]
+       });
+       
+       res.status(200).json(allCategories)
+   } catch (err) {
+       res.status(500).json(err)
+   }
 });
-// finds categories by id
-router.get('/:id', (req, res) => {
 
+// finds categories by id
+router.get('/:id', async (req, res) => {
+   try { 
+       const categoryById = await Category.findByPk(req.params.id, {
+           include: [{ model: Product}],
+       });
+
+       if (!categoryById) {
+           res.status(404).json({ message: 'No category with that ID founf'});
+           return;
+        }
+        res.status(200).json(categoryById)
+   } catch(err) {
+       res.status(500).json(err)
+   }
 });
 
 // creates a new category
-router.post('/', (req, res) =>{
-
+router.post('/', async (req, res) =>{
+    try {
+        const newCategory = await Category.create(req.body);
+        res.status(200).json(newCategory)
+    } catch (err) {
+        res.status(400).json(err)
+    }
 })
 
 // update category by id
