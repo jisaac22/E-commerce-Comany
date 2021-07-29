4cbdb328eb839a858f1574ac9587a2ff970e31e1
jacob sierra <jacobisaac22@yahoo.com>
2021-07-29 00:54:51 -0500

    created code to delete category by id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -65,8 +65,22 @@ router.put('/:id', (req, res) => {
 });
 
 // delete category by id
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
+     try {
+         const deleteCategory = await Category.destroy({
+             where: {
+                 id: req.params.id
+             }
+         });
 
+         if (!deleteCategory) {
+             res.status(404).json({ message: "No category with that ID"})
+             return;
+        }
+        res.status(200).json(deleteCategory);
+     } catch (err){
+         res.status(500).json(err)
+     }
 })
 
 module.exports = router;
